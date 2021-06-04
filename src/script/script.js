9be960dc93e20c1e676054d84a8c347f7d73b2c6
spordict1 <sportdict@gmail.com>
2021-06-04 12:26:14 +0530

    Project Uplaoded on  Git

---
+++ src/script/script.js
@@ -0,0 +1,41 @@
+// Navbar Functioning JS
+var navToggle = document.getElementById('nav-toggle')
+var sideNavContainer = document.querySelector('.container')
+var footer = document.querySelector('.footer')
+navToggle.addEventListener('click', () => {
+	sideNavContainer.classList.toggle('show-nav')
+	footer.classList.toggle('show-nav')
+})
+
+//Split CSS functioning
+
+const splitLeft = document.querySelector('.left')
+const splitRight = document.querySelector('.right')
+const splitContainer = document.querySelector('.split-container')
+if (splitContainer != null) {
+	footer.style.display = 'none'
+}
+splitLeft.addEventListener('mouseenter', () =>
+	splitContainer.classList.add('hover-left')
+)
+splitLeft.addEventListener('mouseleave', () =>
+	splitContainer.classList.remove('hover-left')
+)
+
+splitRight.addEventListener('mouseenter', () =>
+	splitContainer.classList.add('hover-right')
+)
+splitRight.addEventListener('mouseleave', () =>
+	splitContainer.classList.remove('hover-right')
+)
+
+// function openNav() {
+// 	document.getElementById('mySidenav').style.width = '250px'
+// 	document.getElementById('main').style.marginLeft = '250px'
+// }
+
+// /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+// function closeNav() {
+// 	document.getElementById('mySidenav').style.width = '0'
+// 	document.getElementById('main').style.marginLeft = '0'
+// }
